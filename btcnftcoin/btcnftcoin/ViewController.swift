4067260e3f47be2c8162b61f5224b93c37614262
Kaleem <ka.kaleemanwar@gmail.com>
2021-12-05 16:56:36 -0800

    Webview app to load btcnftcoin.com website. Will be updated to load a direct subdomain/url when webapp is available

--- btcnftcoin/btcnftcoin/ViewController.swift
+++ btcnftcoin/btcnftcoin/ViewController.swift
@@ -3,17 +3,27 @@
 //  btcnftcoin
 //
 //  Created by Kaleem on 12/5/21.
-//
+//  Ref: https://developer.apple.com/documentation/webkit/wkwebview
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
+
+    var webView: WKWebView!
+
+    override func loadView() {
+       let webConfiguration = WKWebViewConfiguration()
+       webView = WKWebView(frame: .zero, configuration: webConfiguration)
+       webView.uiDelegate = self
+       view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let myURL = URL(string:"https://btcnftcoin.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
-
-
 }
 
